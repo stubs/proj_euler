195b53d43543133ceaa0b69120606e8bb3e15ef5
Aaron Gonzalez <a.gonzalez979@gmail.com>
2018-02-14 12:27:58 -0500

    initial commit

---
+++ ex1/ex1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+def generate_nats(upper_limit):
+    """generate list of natural numbers below upper_limit that are also multiples of 3 or 5"""
+    return (x for x in xrange(int(upper_limit)) if (x % 3 == 0) or (x % 5 == 0))
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+         The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000""")
+    parser.add_argument("--upper_limit",
+                        help="Calculate the sum of natural numbers which are multiples of 3 or 5 below this upper limit integer",
+                        required=True)
+    args = parser.parse_args()
+
+    print sum(generate_nats(args.upper_limit))
